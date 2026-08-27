7d73fcccd578f6bb7166b9426ee6b857e2b353d1
sharath109 <sharathgoud1447@gmail.com>
2026-08-27 19:45:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const display = document.getElementById("display");
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        const expression = display.value;
+        const result = eval(expression);
+
+        display.value = expression + "=" + result;
+    } catch {
+        display.value = "Error";
+    }
+}
+
+// Keyboard Support
+document.addEventListener("keydown", function (event) {
+
+    const key = event.key;
+
+    if (
+        (key >= "0" && key <= "9") ||
+        key === "+" ||
+        key === "-" ||
+        key === "*" ||
+        key === "/" ||
+        key === "."
+    ) {
+        appendValue(key);
+    }
+
+    else if (key === "Enter") {
+        calculate();
+    }
+
+    else if (key === "Backspace") {
+        deleteLast();
+    }
+
+    else if (key === "Escape") {
+        clearDisplay();
+    }
+
+});
